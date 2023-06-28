2f1f82efd2fa7f6d828554e69f1de2397778fbc8
1Suspensive <jeospinaga@unal.edu.co>
2023-06-28 12:58:35 -0500

    Actividad 6

---
+++ Actividad6/src/main/java/jeferson/actividad6/Actividad6.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package jeferson.actividad6;
+
+/**
+ *
+ * @author JEFERSON
+ */
+public class Actividad6 {
+
+    public static void main(String[] args) {
+        Principal interfaz = new Principal();
+        interfaz.setVisible(true);
+    }
+}
